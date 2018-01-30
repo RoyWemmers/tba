9e333940b8c776cf4af34e085dffbbb5524d211b
Roy Wemmers <D252337@edu.rocwb.nl>
2018-01-30 10:06:03 +0100

    Changes

---
+++ Locations/MainRoad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextAdventureCS
+{
+    class MainRoad : Location
+    {
+        public MainRoad(string name)
+            : base(name)
+        {
+             
+        }
+        public override void Description()
+        {
+            // Insert a nice description
+            Console.WriteLine("You are on the Main Road");
+        }
+
+    }
+}
